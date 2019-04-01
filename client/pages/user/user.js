8fc68db73a5206e9d23bbf73d23e854bea88b512
qwas3110 <283994917@qq.com>
2019-04-01 11:25:43 +0800

    个人登陆静态布局

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -1,66 +1,28 @@
-// pages/user/user.js
+
+
 Page({
-
-	/**
-	 * 页面的初始数据
-	 */
 	data: {
-
+		userInfo: {
+			nickName: 'Laso',
+			avatarUrl: "/images/user-sel.png"
+		},
+		// userInfo: {
+		//	nickName: "Laso",
+		//	avatarUrl: "", // 头像URL地址
+		//}
 	},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-
+	onTapAddress() {
+		wx.showToast({
+			title: '此功能暂未开放',
+			icon: 'none'
+		})
 	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+	
+	onTapKf() {
+		wx.showToast({
+			title: '此功能暂未开放',
+			icon: 'none'
+		})
 	}
 })
