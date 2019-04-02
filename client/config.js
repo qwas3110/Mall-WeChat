3416c491f1f07a1e5b5b0c43bca51ed8dec09514
qwas3110 <283994917@qq.com>
2019-04-02 10:38:53 +0800

    优化代码 订单页面用户登陆授权

--- client/config.js
+++ client/config.js
@@ -3,7 +3,7 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = 'https://lupqvzl3.qcloud.la';
+var host = 'https://lupqvzl3.qcloud.la/';
 
 var config = {
 
@@ -27,7 +27,10 @@ var config = {
 				productList: `${host}/weapp/product`,
 
 				// 获取商品详情
-				productDetail: `${host}/weapp/product/`
+				productDetail: `${host}/weapp/product/`,
+
+				// 拉取用户信息
+				user: `${host}/weapp/user`
     }
 };
 
--- client/pages/order/order.js
+++ client/pages/order/order.js
@@ -1,66 +1,48 @@
-// pages/order/order.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js');
+const config = require('../../config.js');
+
+const app = getApp()
+
+
 Page({
+  data: {
+    userInfo: null,
+    locationAuthType: app.data.locationAuthType
+  },
 
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
+  onLoad() {
 
+  },
+
+	onShow() {
+		this.setData({
+			locationAuthType: app.data.locationAuthType
+		})
+		app.checkSession({
+			success: ({ userInfo }) => {
+				this.setData({
+					userInfo
+				})
+			}
+		})
 	},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
+  onTapLogin: function() {
+    app.login({
+      success: ({
+        userInfo
+      }) => {
+        this.setData({
+          userInfo,
+          locationAuthType: app.data.locationAuthType
+        })
+      },
+      error: () => {
+        this.setData({
+          locationAuthType: app.data.locationAuthType
+        })
+      }
+    })
+  },
 
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
 })
